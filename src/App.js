af30fda45851bf381789f0928413fdca145373e9
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-09-23 19:21:16 +0100

    Reinstated App.js to nav bar and footer

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Navbar from "./components/layout/Navbar";
 import SouperFooter from "./components/layout/SouperFooter";
 import LoginPage from "./components/Login/LoginPage";
 import RegisterPage from "./components/Register/RegisterPage";
-import LandingPage from "./components/LandingPage/LandingPage";
+
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+//import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <RegisterPage />
-    </BrowserRouter>
+    //<BrowserRouter>
+    //  <RegisterPage />
+    //</BrowserRouter>
     //<LoginPage />
     //<LandingPage />
-    //<div className="App">
-    //  <Navbar />
-    //  <SouperFooter />
-    //</div>
+    <div className="App">
+      <Navbar />
+      <SouperFooter />
+    </div>
   );
 }
 
