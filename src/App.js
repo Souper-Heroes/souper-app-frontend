29de91dd80cfaad5727ae4dfe1395efd4a168873
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-09-23 20:14:06 +0100

    revert App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from "react";
 import Navbar from "./components/layout/Navbar";
 import SouperFooter from "./components/layout/SouperFooter";
 import LoginPage from "./components/Login/LoginPage";
-import RegisterPage from "./components/Register/RegisterPage";
-
+//import RegisterPage from "./components/Register/RegisterPage";
+//import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./App.css";
-//import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    //<BrowserRouter>
-    //  <RegisterPage />
-    //</BrowserRouter>
     //<LoginPage />
-    //<LandingPage />
+    //<RegisterPage />
     <div className="App">
       <Navbar />
       <SouperFooter />
