99a4c484c455f29a98874d2a399c542768571729
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-09-23 18:24:24 +0100

    Added Register page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-
-import React from 'react';
-import Navbar from './components/layout/Navbar';
-import SouperFooter from './components/layout/SouperFooter';
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import SouperFooter from "./components/layout/SouperFooter";
 import LoginPage from "./components/Login/LoginPage";
-import './App.css';
-
+import RegisterPage from "./components/Register/RegisterPage";
+import LandingPage from "./components/LandingPage/LandingPage";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
+      <RegisterPage />
+    </BrowserRouter>
     //<LoginPage />
-    <div className="App">
-      <Navbar />
-      <SouperFooter />
-    </div>
+    //<LandingPage />
+    //<div className="App">
+    //  <Navbar />
+    //  <SouperFooter />
+    //</div>
   );
 }
 
