ba36c4fb96e0f43f96da8deeada654cb7f36dd95
Andy Styles <Styles@caytonslaw.com>
2020-09-23 18:54:59 +0100

    initial commit for navbar component development

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Header from '../MaterialKitComponents/Header/Header.js';
-import styles from '../../assets/jss/material-kit-react/components/headerStyle.js';
+import { Restaurant } from '@material-ui/icons';
+import Header from 'components/MaterialKitComponents/Header/Header.js';
+import HeaderLinks from 'components/MaterialKitComponents/Header/HeaderLinks.js';
 
 function Navbar() {
-  return <Header brand='Brand' color='rose' />;
+  return (
+    <Header
+      brand={<Restaurant />}
+      color='rose'
+      leftLinks={''}
+      rightLinks={<HeaderLinks />}
+    />
+  );
 }
 
 export default Navbar;
