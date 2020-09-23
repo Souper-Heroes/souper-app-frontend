12b9122d96e65f23d7028f4aa4953f0e1119cb02
Abi Sidford <asidford@gmail.com>
2020-09-23 13:16:23 +0100

    Added a custom footer - SouperFooter.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import SouperFooter from './components/layout/SouperFooter';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <SouperFooter />
     </div>
   );
 }
