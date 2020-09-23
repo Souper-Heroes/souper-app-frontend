7d3d703f779749c3bb4f66098f71e7b9668f9f86
Andy Styles <Styles@caytonslaw.com>
2020-09-23 09:56:03 +0100

    bring in material ui react kit

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,15 +2,11 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Header from '../MaterialKitComponents/Header/Header.js';
+import styles from '../../assets/jss/material-kit-react/components/headerStyle.js';
 
 function Navbar() {
-  return (
-    <AppBar position='static'>
-      <Toolbar>
-        <Button color='inherit'>Login</Button>
-      </Toolbar>
-    </AppBar>
-  );
+  return <Header brand='Brand' color='rose' />;
 }
 
 export default Navbar;
