6adf455bea45c3f5ebd122b74ee6a1f540e1d8b6
cavalost <claudina.avalos@opendeusto.es>
2020-09-24 13:15:28 +0100

    feat: login page styles

--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -5,7 +5,6 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
 // core components
 import GridContainer from "components/MaterialKitComponents/Grid/GridContainer.js";
 import GridItem from "components/MaterialKitComponents/Grid/GridItem.js";
@@ -13,14 +12,14 @@ import Button from "components/MaterialKitComponents/CustomButtons/Button.js";
 import Card from "components/MaterialKitComponents/Card/Card.js";
 import CardBody from "components/MaterialKitComponents/Card/CardBody.js";
 import CardHeader from "components/MaterialKitComponents/Card/CardHeader.js";
+
 import CardFooter from "components/MaterialKitComponents/Card/CardFooter.js";
 import CustomInput from "components/MaterialKitComponents/CustomInput/CustomInput.js";
 
 import styles from "assets/jss/material-kit-react/views/loginPage.js";
-//import Image from  '/Documentation/assets/img/board.jpg'; // Import using relative path
+import image from "assets/img/board.jpg";
 
-import image from "assets/img/bg7.jpg";
-import { Redirect, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(styles);
 
@@ -33,13 +32,20 @@ export default function LoginPage(props) {
   const { ...rest } = props;
   return (
     <div>
-
+      <div
+          style={{
+            backgroundImage: "url(" + image + ")",
+            backgroundSize: "cover",
+            backgroundPosition: "top center",
+          }}
+      >
         <div className={classes.container}>
+
           <GridContainer justify="center">
             <GridItem xs={12} sm={12} md={4}>
               <Card className={classes[cardAnimaton]}>
                 <form className={classes.form}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
+                  <CardHeader color="rose" className={classes.cardHeader}>
                     <h4>Login</h4>
                     <div className={classes.socialLine}>
                       <Button
@@ -106,17 +112,38 @@ export default function LoginPage(props) {
                       }}
                     />
                   </CardBody>
+                  <GridContainer justify="center">
+                    <Link to="/ForgottenPassword" className={classes.link}>
+                      <Button
+                          simple
+                          color="info"
+                          size="lg"
+                          to="/ForgottenPassword"
+                      >
+                        FORGOTTEN PASSWORD?
+                      </Button>
+                    </Link>
+                  </GridContainer>
                   <CardFooter className={classes.cardFooter}>
                     <Button variant="contained" color="rose" size="lg">
-                      <NavLink to="/register" activeClassName="hurray">
+                      <Link to="/">
                         LOG IN
-                      </NavLink>
+                      </Link>
+                    </Button>
+                  </CardFooter>
+                  <CardFooter className={classes.cardFooter}>
+                    <Button variant="contained" color="default" size="lg">
+                      <Link to="/register">
+                        REGISTER
+                      </Link>
                     </Button>
                   </CardFooter>
                 </form>
               </Card>
             </GridItem>
           </GridContainer>
+
+          </div>
         </div>
     </div>
   );
