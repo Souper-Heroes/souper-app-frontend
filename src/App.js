333b905fde22679b3d2f5a1c261ac92798554440
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-09-24 11:30:26 +0100

    Moved RegisterPage to Login folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
 import LoginPage from "./components/Login/LoginPage";
-import RegisterPage from "./components/Register/RegisterPage";
+import RegisterPage from "./components/Login/RegisterPage";
 import Page from "./components/Page";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-      <BrowserRouter>
-          <Switch>
-              <Route path="/login">
-                  <LoginPage />
-              </Route>
-              <Route path="/register">
-                  <RegisterPage />
-              </Route>
-              <Route path="/">
-                  <Page />
-              </Route>
-          </Switch>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/register">
+          <RegisterPage />
+        </Route>
+        <Route path="/">
+          <Page />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
