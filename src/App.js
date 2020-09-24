0f47e96441edfab43cbba2a3c6637cc0a5215d85
cavalost <claudina.avalos@opendeusto.es>
2020-09-24 09:37:05 +0100

    feat: add routing to login page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
-import Navbar from "./components/layout/Navbar";
-import SouperFooter from "./components/layout/SouperFooter";
 import LoginPage from "./components/Login/LoginPage";
-//import RegisterPage from "./components/Register/RegisterPage";
-//import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import RegisterPage from "./components/Register/RegisterPage";
+import Page from "./components/Page";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    //<LoginPage />
-    //<RegisterPage />
-    <div className="App">
-      <Navbar />
-      <SouperFooter />
-    </div>
+      <BrowserRouter>
+          <Switch>
+              <Route path="/login">
+                  <LoginPage />
+              </Route>
+              <Route path="/register">
+                  <RegisterPage />
+              </Route>
+              <Route path="/">
+                  <Page />
+              </Route>
+          </Switch>
+      </BrowserRouter>
   );
 }
 
