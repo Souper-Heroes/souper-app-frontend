c1915269360b10f283e83e5600367b802953a79e
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-09-24 12:30:57 +0100

    Use Single Quotes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import LoginPage from "./components/Login/LoginPage";
-import RegisterPage from "./components/Login/RegisterPage";
-import Page from "./components/Page";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import LoginPage from './components/Login/LoginPage';
+import RegisterPage from './components/Login/RegisterPage';
+import Page from './components/Page';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/login">
+        <Route path='/login'>
           <LoginPage />
         </Route>
-        <Route path="/register">
+        <Route path='/register'>
           <RegisterPage />
         </Route>
-        <Route path="/">
+        <Route path='/'>
           <Page />
         </Route>
       </Switch>
