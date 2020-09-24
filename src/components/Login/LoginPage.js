147948a6afa0e5eedd846b8c5f40fb603fe1583a
cavalost <claudina.avalos@opendeusto.es>
2020-09-24 14:24:48 +0100

    feat: add eslint configuration and fix LoginPage

--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -1,48 +1,44 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
+import { makeStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Icon from '@material-ui/core/Icon';
 // @material-ui/icons
-import Email from "@material-ui/icons/Email";
+import Email from '@material-ui/icons/Email';
 // core components
-import GridContainer from "components/MaterialKitComponents/Grid/GridContainer.js";
-import GridItem from "components/MaterialKitComponents/Grid/GridItem.js";
-import Button from "components/MaterialKitComponents/CustomButtons/Button.js";
-import Card from "components/MaterialKitComponents/Card/Card.js";
-import CardBody from "components/MaterialKitComponents/Card/CardBody.js";
-import CardHeader from "components/MaterialKitComponents/Card/CardHeader.js";
+import GridContainer from '../MaterialKitComponents/Grid/GridContainer.js';
+import GridItem from '../MaterialKitComponents/Grid/GridItem.js';
+import Button from '../MaterialKitComponents/CustomButtons/Button.js';
+import Card from '../MaterialKitComponents/Card/Card.js';
+import CardBody from '../MaterialKitComponents/Card/CardBody.js';
+import CardHeader from '../MaterialKitComponents/Card/CardHeader.js';
+import CardFooter from '../MaterialKitComponents/Card/CardFooter.js';
+import CustomInput from '../MaterialKitComponents/CustomInput/CustomInput.js';
 
-import CardFooter from "components/MaterialKitComponents/Card/CardFooter.js";
-import CustomInput from "components/MaterialKitComponents/CustomInput/CustomInput.js";
-
-import styles from "assets/jss/material-kit-react/views/loginPage.js";
-import image from "assets/img/board.jpg";
-
-import { Link } from "react-router-dom";
+import styles from '../../assets/jss/material-kit-react/views/loginPage.js';
+import image from '../../assets/img/board.jpg';
 
 const useStyles = makeStyles(styles);
 
-export default function LoginPage(props) {
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function () {
-    setCardAnimation("");
+export default function LoginPage() {
+  const [cardAnimaton, setCardAnimation] = React.useState('cardHidden');
+  setTimeout(() => {
+    setCardAnimation('');
   }, 700);
   const classes = useStyles();
-  const { ...rest } = props;
   return (
     <div>
       <div
-          style={{
-            backgroundImage: "url(" + image + ")",
-            backgroundSize: "cover",
-            backgroundPosition: "top center",
-          }}
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'top center',
+        }}
       >
         <div className={classes.container}>
-
           <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={4}>
+            <GridItem xs="12" sm="12" md="4">
               <Card className={classes[cardAnimaton]}>
                 <form className={classes.form}>
                   <CardHeader color="rose" className={classes.cardHeader}>
@@ -55,7 +51,7 @@ export default function LoginPage(props) {
                         color="transparent"
                         onClick={(e) => e.preventDefault()}
                       >
-                        <i className={"fab fa-twitter"} />
+                        <i className="fab fa-twitter" />
                       </Button>
                       <Button
                         justIcon
@@ -64,7 +60,7 @@ export default function LoginPage(props) {
                         color="transparent"
                         onClick={(e) => e.preventDefault()}
                       >
-                        <i className={"fab fa-facebook"} />
+                        <i className="fab fa-facebook" />
                       </Button>
                       <Button
                         justIcon
@@ -73,7 +69,7 @@ export default function LoginPage(props) {
                         color="transparent"
                         onClick={(e) => e.preventDefault()}
                       >
-                        <i className={"fab fa-google-plus-g"} />
+                        <i className="fab fa-google-plus-g" />
                       </Button>
                     </div>
                   </CardHeader>
@@ -85,7 +81,7 @@ export default function LoginPage(props) {
                         fullWidth: true,
                       }}
                       inputProps={{
-                        type: "email",
+                        type: 'email',
                         endAdornment: (
                           <InputAdornment position="end">
                             <Email className={classes.inputIconsColor} />
@@ -100,7 +96,7 @@ export default function LoginPage(props) {
                         fullWidth: true,
                       }}
                       inputProps={{
-                        type: "password",
+                        type: 'password',
                         endAdornment: (
                           <InputAdornment position="end">
                             <Icon className={classes.inputIconsColor}>
@@ -108,18 +104,13 @@ export default function LoginPage(props) {
                             </Icon>
                           </InputAdornment>
                         ),
-                        autoComplete: "off",
+                        autoComplete: 'off',
                       }}
                     />
                   </CardBody>
                   <GridContainer justify="center">
                     <Link to="/ForgottenPassword" className={classes.link}>
-                      <Button
-                          simple
-                          color="info"
-                          size="lg"
-                          to="/ForgottenPassword"
-                      >
+                      <Button simple color="info" size="lg" to="/ForgottenPassword">
                         FORGOTTEN PASSWORD?
                       </Button>
                     </Link>
@@ -142,9 +133,8 @@ export default function LoginPage(props) {
               </Card>
             </GridItem>
           </GridContainer>
-
-          </div>
         </div>
+      </div>
     </div>
   );
 }
