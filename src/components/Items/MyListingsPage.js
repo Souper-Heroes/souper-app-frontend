22133cad82faf1ca1fe8d387c258d10f768d50e6
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-09-27 12:27:54 +0100

    Refactored file structure

--- src/components/Items/MyListingsPage.js
+++ src/components/Items/MyListingsPage.js
@@ -1,17 +1,11 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import Header from 'components/Layout/Header.js';
-import HeaderLinks from 'components/Layout/HeaderLinks.js';
-import { Restaurant } from '@material-ui/icons';
-import SouperFooter from 'components/Layout/SouperFooter';
-
 import classNames from 'classnames';
 import { makeStyles } from '@material-ui/core/styles';
 import GridContainer from 'components/MaterialKitComponents/Grid/GridContainer.js';
 import GridItem from 'components/MaterialKitComponents/Grid/GridItem.js';
-import Button from 'components/CustomButtons/Button.js';
 import styles from 'assets/jss/material-kit-react/views/profilePage.js';
-import ListingTabSection from './Sections/ListingTabSection.js';
+import TabListings from 'components/Items/TabListings.js';
 
 const useStyles = makeStyles(styles);
 
@@ -24,10 +18,7 @@ function MyListingsPage({ userProfile, userItems }) {
       <div className={classes.container}>
         <GridContainer justify='center'>
           <GridItem xs={12} sm={12} md={8} lg={8}>
-            <ListingTabSection
-              userProfile={userProfile}
-              userItems={userItems}
-            />
+            <TabListings userProfile={userProfile} userItems={userItems} />
           </GridItem>
         </GridContainer>
       </div>
