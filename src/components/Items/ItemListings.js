37ac219bbfaf3ee1c736ed11a05d9b1956695bc9
Your Correct Name <andystyles30@gmail.com>
2020-09-28 18:55:34 +0100

    add styles and start building the filter options on the item listings component

--- src/components/Items/ItemListings.js
+++ src/components/Items/ItemListings.js
@@ -1,35 +1,183 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Datetime from "react-datetime";
 import classNames from 'classnames';
 import { makeStyles } from '@material-ui/core/styles';
 import GridContainer from 'components/MaterialKitComponents/Grid/GridContainer.js';
 import GridItem from 'components/MaterialKitComponents/Grid/GridItem.js';
-import Button from 'components/CustomButtons/Button.js';
 
 import styles from 'assets/jss/material-kit-react/views/profilePage.js';
-import profile from 'assets/img/faces/christian.jpg';
 import Card from 'components/MaterialKitComponents/Card/Card.js';
 import CardBody from 'components/MaterialKitComponents/Card/CardBody.js';
+import CustomInput from "components/MaterialKitComponents/CustomInput/CustomInput.js";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Button from 'components/MaterialKitComponents/CustomButtons/Button.js';
 
 const useStyles = makeStyles(styles);
 
 function Page() {
   const classes = useStyles();
+  const [unit, setUnit] = useState('');
+
+  const handleChange = (event) => {
+    setUnit(event.target.value);
+  };
   return (
     <div className={classNames(classes.main, classes.mainRaised)}>
       <div>
         <div className={classes.container}>
           <GridContainer>
-            <GridItem xs={12} sm={4} md={3} container justify="center">
-              <GridItem xs={12} sm={6} md={12}>
-                <div className={classes.profile}>
-                  <div className={classes.name}>
-                    <h3 className={classes.title}>Christian Louboutin</h3>
-                    <h6>DESIGNER</h6>
-                  </div>
-                </div>
+            <GridItem xs={12} sm={4} md={3} container spacing={1}>
+              <GridItem xs={12} sm={12} md={12} >
+                <h4 className={classes.filterTitle}>Filters</h4>
+                <InputLabel className={classes.filterLabel}>
+                  Expiry Date
+                </InputLabel>
+                <FormControl fullWidth>
+                  <Datetime
+                    inputProps={{ placeholder: "Select Expiry date.." }}
+                  />
+                </FormControl>
+                <FormControl fullWidth>
+                  <Button fullWidth size='md' color='rose'>
+                    Apply Filters
+                    </Button>
+                </FormControl>
               </GridItem>
             </GridItem>
-            <GridItem xs={12} sm={8} md={9} container direction="row">
+            <GridItem xs={12} sm={8} md={9} container direction="row" spacing={1}>
+              <GridItem xs={12} sm={6} md={4} >
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={6} md={4}>
+                <Card>
+                  <CardBody>
+                    <h4 className={classes.cardTitle}>Card title</h4>
+                    <p>
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                  </p>
+                  </CardBody>
+                </Card>
+              </GridItem>
               <GridItem xs={12} sm={6} md={4}>
                 <Card>
                   <CardBody>
@@ -97,6 +245,7 @@ function Page() {
                 </Card>
               </GridItem>
             </GridItem>
+
           </GridContainer>
 
           <div className={classes.description}>
