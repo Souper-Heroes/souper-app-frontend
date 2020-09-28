09060de7bde37f00b84f9a16250b7c275e37615b
Abi Sidford <asidford@gmail.com>
2020-09-28 14:25:17 +0100

    Fighting with DatePicker, progress but not finished

--- src/components/AddEditItem.js
+++ src/components/AddEditItem.js
@@ -1,18 +1,17 @@
-import React from "react";
-// Get dateTime
-import Datetime from "react-datetime";
+import React from 'react';
+
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 import { Select } from '@material-ui/core';
 import MenuItem from '@material-ui/core/MenuItem';
-
-
+import TextField from '@material-ui/core/TextField';
 // material-ui components
 
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
+import InputLabel from '@material-ui/core/InputLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 // Material Kit components
 import Parallax from "./MaterialKitComponents/Parallax/Parallax"
@@ -20,59 +19,47 @@ import Button from "./MaterialKitComponents/CustomButtons/Button.js";
 import GridContainer from "./MaterialKitComponents/Grid/GridContainer.js";
 import GridItem from "./MaterialKitComponents/Grid/GridItem.js";
 import CustomInput from "./MaterialKitComponents/CustomInput/CustomInput.js";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-// @material-ui/icons
-import Check from "@material-ui/icons/Check";
-// assets
-import FoodPic from "assets/img/purple-banana.jpg";
 
-//TODO - this uses files from views will have to get styles from somewhere else
+// @material-ui/icons
+// import Check from "@material-ui/icons/Check";
+// assets
+import FoodPic from '../assets/img/purple-banana.jpg';
+
+// TODO - this uses files from views will have to get styles from somewhere else
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
+import { CalendarToday } from '@material-ui/icons';
 const useStyles = makeStyles(styles);
 
 
-
 export default function AddEditItem(props) {
-  const [checked, setChecked] = React.useState([24, 22]);  
+  
   const classes = useStyles();
-
-  const wrapperDiv = classNames(
-    classes.checkboxAndRadio,
-    classes.checkboxAndRadioHorizontal
-  );  
-  const handleToggle = value => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-    setChecked(newChecked);
-  };
-
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
 
+  
   return (
     <div>
       <Parallax small filter image={require("assets/img/unsplash-redfruit.jpg")} />
       <div className={classNames(classes.main, classes.mainRaised)}>
-        <div>
+       
           <div className={classes.container}>
             <GridContainer justify="center">
               <GridItem xs={12} sm={6} className={classes.navWrapper}>
-                <img        
-                    src={FoodPic}
-                    alt="Bananas"
-                    className={navImageClasses}  
-                />
+                <a href="#LinkToChangeThePic">
+                  <img        
+                      src={FoodPic}
+                      alt="Bananas"
+                      className={navImageClasses}  
+                  />
+                </a>
               </GridItem>
               <GridItem xs={12} sm={6} className={classes.navWrapper}>
                 <CustomInput
                   labelText="Title"
                   id="float"
+                  inputProps={{
+                    placeholder: "Give your item a name"
+                  }}
                   formControlProps={{
                       fullWidth: true
                   }}
@@ -80,6 +67,9 @@ export default function AddEditItem(props) {
                 <CustomInput
                     labelText="Description"
                     id="float"
+                    inputProps={{
+                      placeholder: "Describe your item, has it been opened or dropped?"
+                    }}
                     formControlProps={{
                         fullWidth: true
                     }}
@@ -94,51 +84,67 @@ export default function AddEditItem(props) {
                 </div>
               </GridItem>
             </GridContainer>
-
             <GridContainer justify="center">
               <GridItem xs={12} sm={6} className={classes.navWrapper}>
-                <div>
-                    <InputLabel className={classes.label}>
-                        Enter the Date and Time the Item is available from.
-                    </InputLabel>
-                    <br />
-                    <FormControl fullWidth>
-                        <Datetime
-                        inputProps={{ placeholder: "Datetime Picker Here" }}
-                        />
-                    </FormControl>
-                </div>     
+              <TextField
+                  id="date"
+                  label="Expiry date"
+                  type="date"
+                  defaultValue={CalendarToday}
+                  fullWidth
+                  className={classes.textField}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />    
                <CustomInput
                     labelText="Location"
                     id="float"
+                    inputProps={{
+                      placeholder: "Enter a Postcode"
+                    }}
                     formControlProps={{
                         fullWidth: true
                     }}
                  />   
-                <div className={wrapperDiv}>
-                    <FormControlLabel
-                    control={
-                        <Checkbox
-                        tabIndex={-1}
-                        onClick={() => handleToggle(21)}
-                        checkedIcon={<Check className={classes.checkedIcon} />}
-                        icon={<Check className={classes.uncheckedIcon} />}
-                        classes={{ checked: classes.checked }}
-                        />
-                    }
-                    classes={{ label: classes.label }}
-                    label="Same location as profile?"
-                    />
-                </div>
+                <FormControlLabel 
+                  control={
+                    <Checkbox name="locationSameProfile" />} 
+                    label="Use location set in User profile?" 
+                /> 
               </GridItem>
               <GridItem xs={12} sm={6} className={classes.navWrapper}>
-               <h3>This is number two!</h3>
+                <p>Available collection time</p>
+                <GridContainer justify="center">
+                  <GridItem xs={12} sm={6} className={classes.navWrapper}>
+                      <TextField
+                        id="date"
+                        label="From:"
+                        type="date"
+                        defaultValue={CalendarToday}    
+                        className={classes.textField}
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                      />      
+                  </GridItem>
+                  <GridItem xs={12} sm={6} className={classes.navWrapper}>
+                      <TextField
+                        id="date"
+                        label="To:"
+                        type="date"
+                        defaultValue={CalendarToday}
+                        className={classes.textField}
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                      /> 
+                  </GridItem>
+                </GridContainer>
               </GridItem>
             </GridContainer>
           </div>
-        </div>
       </div>
-      
     </div>
   );
 }
