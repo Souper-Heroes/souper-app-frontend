00550a0714844047075798cdb9fe0952499036a1
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-10-02 17:00:28 +0100

    Add Event handling on sort by

--- src/components/Items/MyItemListings.jsx
+++ src/components/Items/MyItemListings.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/jsx-tag-spacing */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useState } from 'react';
 // @material-ui/core components
-// import { makeStyles } from '@material-ui/core/styles';
-
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 // @material-ui/icons
 // import Chat from '@material-ui/icons/Chat';
 // import VerifiedUser from '@material-ui/icons/VerifiedUser';
@@ -19,42 +19,59 @@ import ListingsDropdown from 'components/Layout/ListingsDropdown';
 import Button from 'components/CustomButtons/Button';
 import ListingsPagination from '../Layout/ListingsPagination';
 
-// import styles from 'assets/jss/Items/views/MyItemListings';
+//import styles from 'assets/jss/Items/views/MyItemListings';
+import styles from 'assets/jss/material-kit-react/views/loginPage.js';
 // import Divider from '@material-ui/core/Divider';
 
 // const useStyles = makeStyles(styles);
 
 export default function MyItemListings({ type, myitems }) {
-  // const classes = useStyles();
+  const [items, setItems] = useState(myitems);
+
+  const classes = makeStyles(styles);
+  const paginationColSize = type === 'provide' ? 6 : 12;
+
+  const sortItems = (menuItem) => {
+    const newItems = [...items];
+
+    if (menuItem === 'Category') {
+      newItems.sort((a, b) =>
+        a.category > b.category ? 1 : b.category > a.category ? -1 : 0
+      );
+    } else {
+      //  Sort By Expiry Date
+      newItems.sort((a, b) =>
+        a.expiryDate > b.expiryDate ? 1 : b.expiryDate > a.expiryDate ? -1 : 0
+      );
+    }
+    console.log('Sorted Items:', newItems);
+
+    setItems(newItems);
+  };
 
   return (
     <div /* {className={classes.section} } */>
       <GridContainer>
-        <GridItem align="right">
-          <ListingsDropdown />
+        <GridItem align='right'>
+          <ListingsDropdown sortItems={sortItems} />
         </GridItem>
         <GridItem xs={12} sm={12} md={12}>
-          {myitems.map((myitem) => (
-            <MyItemListing key={myitem.itemId} type={type} myitem={myitem} />
+          {items.map((myItem) => (
+            <MyItemListing key={myItem.itemId} type={type} myitem={myItem} />
           ))}
         </GridItem>
         {type === 'provide' && (
-          <GridItem xs={6} align="left">
-            <Button type="button" color="rose">
-              Add Item
-            </Button>
+          <GridItem xs={6} align='left'>
+            <Link to='/addedititem' className={classes.link}>
+              <Button type='button' color='rose'>
+                Add Item
+              </Button>
+            </Link>
           </GridItem>
         )}
-        {type === 'provide' && (
-          <GridItem xs={6} align="right">
-            <ListingsPagination />
-          </GridItem>
-        )}
-        {type !== 'provide' && (
-          <GridItem xs={12} align="right">
-            <ListingsPagination />
-          </GridItem>
-        )}
+        <GridItem xs={paginationColSize} align='right'>
+          <ListingsPagination />
+        </GridItem>
       </GridContainer>
     </div>
   );
