1d0d68b295f31707f9a6eadf305f9d95b946a993
cavalost <claudina.avalos@opendeusto.es>
2020-10-05 13:50:43 +0100

    feat: change SoupAPI and logout action

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,7 +1,9 @@
 import { types } from 'actions';
 
-const auth = (state = { jwt: '', message: '', isLogged: false }, action) => {
-    const newState = {...state};
+const initialState = { jwt: '', message: '', isLogged: false };
+
+const auth = (state = {...initialState}, action) => {
+    let newState = {...state};
     switch (action.type) {
         case `${types.LOGIN}_FULFILLED`:
             newState.jwt = action.payload.data.token;
@@ -9,6 +11,9 @@ const auth = (state = { jwt: '', message: '', isLogged: false }, action) => {
             break;
         case `${types.CHECK}_FULFILLED`:
             newState.message = action.payload.data.message;
+            break;
+        case types.LOGOUT:
+            newState = {...initialState};
             break;
     }
     return newState;
