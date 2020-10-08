bf6523c9dce1574465b47ebbcbaf93ef0590c566
Your Correct Name <andystyles30@gmail.com>
2020-10-08 15:39:45 +0100

    update actions for auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,8 @@
-import axios from 'axios';
-
-const SOUP_API = 'https://souper-app-backend.herokuapp.com';
-
 export const types = {
-    UPDATE_MESSAGE: 'UPDATE_MESSAGE',
-    LOGIN: 'LOGIN',
-    LOGOUT:'LOGOUT',
-    CHECK: 'CHECK'
+  UPDATE_MESSAGE: 'UPDATE_MESSAGE'
 };
 
-export const login = (email, password) => ({
-    type: types.LOGIN,
-    payload: axios.post(`${SOUP_API}/login`, {email, password})
-});
-
-export const logout = () => dispatch => dispatch({
-    type: types.LOGOUT
-});
-
-export const check = () => (dispatch, getState) => dispatch(({
-    type: types.CHECK,
-    payload: axios.get(`${SOUP_API}/check`, {headers: { Authorization: getState().auth.jwt}})
-}));
-
 export const updateMessage = text => ({
-    type: types.UPDATE_MESSAGE,
-    text
+  type: types.UPDATE_MESSAGE,
+  text
 });
