8de8bb134172703ca3819d0c2425517187e62213
Andy Styles <Styles@caytonslaw.com>
2020-10-09 18:43:54 +0100

    map authentication state to layout, might be useful

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,7 +2,9 @@ import { connect } from 'react-redux';
 import Layout from 'components/Layout/Layout';
 
 const mapStateToProps = state => ({
-  isLogged: null
+  isLoggingIn: state.auth.isLoggingIn,
+  loginError: state.auth.loginError,
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps)(Layout);
