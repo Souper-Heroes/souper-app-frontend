056c4ab98f601386a585e42d4fb0124f69a93050
Your Correct Name <andystyles30@gmail.com>
2020-10-11 17:07:33 +0100

    tidy up

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,20 @@ import 'assets/scss/material-kit-react.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import App from './App';
-import configureStore from './configureStore';
+import configureStore from './store';
 import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import message from './message';
 import auth from './auth';
 
-export default combineReducers({ message, auth });
+const persistConfig = {
+  key: 'auth',
+  storage
+};
+
+export default combineReducers({
+  message,
+  auth: persistReducer(persistConfig, auth)
+});
