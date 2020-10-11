467f8612476eff244238143ac4a483724b054ed7
Andy Styles <Styles@caytonslaw.com>
2020-10-11 18:01:05 +0100

    uppercase R in folder name Routing

---
+++ src/components/Routing/ProtectedRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import Layout from 'containers/Layout/Layout';
+
+const ProtectedRoute = ({
+  component: Component,
+  isAuthenticated,
+  isVerifying,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      isVerifying ? (
+        <div />
+      ) : isAuthenticated ? (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+export default ProtectedRoute;
