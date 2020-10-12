88f81cd0e73d3f4eb0f8f27b811d600db96b8c25
Your Correct Name <andystyles30@gmail.com>
2020-10-12 11:25:45 +0100

    tidy up item data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage';
 
 import message from './message';
 import auth from './auth';
+import item from './item';
 
 const persistConfig = {
   key: 'auth',
@@ -11,6 +12,7 @@ const persistConfig = {
 };
 
 export default combineReducers({
+  item,
   message,
   auth: persistReducer(persistConfig, auth)
 });
