6a183a1cc0b555beae49dbd5c8d2e0f24b7c253d
Your Correct Name <andystyles30@gmail.com>
2020-10-13 09:30:54 +0100

    add utils - interceptor for api requests - add firebase jwt header and baseURL

--- src/actions/item.js
+++ src/actions/item.js
@@ -1,34 +1,18 @@
-import { myFirebase } from '../firebase/firebase';
-import axios from 'axios';
+import api from '../utils/api';
 
 export const types = {
   GET_ITEMS: 'GET_ITEMS',
   GET_ITEMS_ERROR: 'GET_ITEMS_ERROR'
 };
 
-const SOUP_API = 'http://localhost:5000';
-
-// const SOUP_API = 'https://souper-app-backend.herokuapp.com';
-
 export const getItems = () => async dispatch => {
   try {
-    myFirebase
-      .auth()
-      .currentUser.getIdToken()
-      .then(async idToken => {
-        const res = await axios.get(`${SOUP_API}/api/items`, {
-          headers: { 'x-auth-token': idToken }
-        });
-        dispatch({
-          type: types.GET_ITEMS,
-          payload: res.data
-        });
-      })
-      .catch(error => {
-        dispatch({
-          type: types.GET_ITEMS_ERROR
-        });
-      });
+    const res = await api.get('/items');
+
+    dispatch({
+      type: types.GET_ITEMS,
+      payload: res.data
+    });
   } catch (err) {
     // do something with error
     console.log(err);
