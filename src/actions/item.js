bab1edc8a3ad958c6a7476f241b151ff5b6e9bb6
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-10-15 10:40:57 +0100

    Merge changes from development

--- src/actions/item.js
+++ src/actions/item.js
@@ -5,12 +5,9 @@ export const types = {
   GET_ITEMS_ERROR: 'GET_ITEMS_ERROR',
   DELETEITEM: 'DELETEITEM',
   SORTITEM: 'SORTITEM',
-<<<<<<< HEAD
   UPDATE_COLLECTDATES: 'UPDATE_COLLECTDATES',
   RESERVE_ITEM: 'RESERVE_ITEM',
   UNRESERVE_ITEM: 'UNRESERVE_ITEM',
-=======
-  UPDATE_COLLECTDATES: 'UPDATE_COLLECTDATES'
 };
 
 export const getItems = () => async dispatch => {
@@ -19,44 +16,32 @@ export const getItems = () => async dispatch => {
 
     dispatch({
       type: types.GET_ITEMS,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     // do something with error
     console.log(err);
     dispatch({
-      type: types.GET_ITEMS_ERROR
+      type: types.GET_ITEMS_ERROR,
     });
   }
->>>>>>> e8a8e3737108682483865c72aaad390e23f51fae
 };
 
 export const deleteItem = _id => ({
   type: types.DELETEITEM,
-<<<<<<< HEAD
   _id,
-=======
-  itemId
->>>>>>> e8a8e3737108682483865c72aaad390e23f51fae
 });
 
 export const sortByItem = menuItem => ({
   type: types.SORTITEM,
-  menuItem
+  menuItem,
 });
 
 export const updateCollectionDates = (_id, availability) => ({
   type: types.UPDATE_COLLECTDATES,
-<<<<<<< HEAD
   _id,
   availability,
   success: true,
-=======
-  itemId,
-  collectionStartDateTime,
-  collectionEndDateTime,
-  success: true
->>>>>>> e8a8e3737108682483865c72aaad390e23f51fae
 });
 
 export const reserveItem = (_id, itemId) => ({
