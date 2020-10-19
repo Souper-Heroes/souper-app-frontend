3b4836d33799bffe5b32cb31b1fede544f32df13
Abi Sidford <asidford@gmail.com>
2020-10-19 10:50:53 +0100

    Finished Linting

--- src/containers/Items/AddEditItem.js
+++ src/containers/Items/AddEditItem.js
@@ -10,7 +10,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
 
-  addItem: (formData) => dispatch(addItem(formData))
+  addItem: formData => dispatch(addItem(formData))
 
 });
 
