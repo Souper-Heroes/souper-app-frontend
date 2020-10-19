30cc44bfb5aa27dc1542898c247e5017eb6254c9
cavalost <claudina.avalos@opendeusto.es>
2020-10-19 10:50:58 +0100

    feat: revert profile text just for testing

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -57,7 +57,7 @@ export default function Profile() {
         <GridContainer justify="center">
           <GridItem cs={12} sm={12} md={8}>
             <h2 className={classes.title}>
-              Profile1
+              Profile
             </h2>
             <form>
               <GridContainer>
