ad76849440274a34e458a8a3513dbb6d213b13bb
Abi Sidford <asidford@gmail.com>
2020-10-20 12:20:47 +0100

    Added reducer and actions

--- src/actions/item.js
+++ src/actions/item.js
@@ -4,7 +4,9 @@ export const types = {
   ADD_ITEM: 'ADD_ITEM',
   ADD_ITEM_ERROR: 'ADD_ITEM_ERROR',
   ADD_ITEM_RESET_FORM: 'ADD_ITEM_RESET_FORM',
+  GET_ITEM: 'GET_ITEM',
   GET_ITEMS: 'GET_ITEMS',
+  GET_ITEM_ERROR: 'GET_ITEM_ERROR',
   GET_ITEMS_ERROR: 'GET_ITEMS_ERROR',
   GET_MY_ITEMS: 'GET_MY_ITEMS',
   GET_MY_ITEMS_ERROR: 'GET_MY_ITEMS_ERROR',
@@ -15,7 +17,7 @@ export const types = {
   UPDATE_COLLECTDATES: 'UPDATE_COLLECTDATES',
   RESERVE_ITEM: 'RESERVE_ITEM',
   UNRESERVE_ITEM: 'UNRESERVE_ITEM',
-  UNRESERVE_ITEM_ERROR: 'UNRESERVE_ITEM_ERROR'
+  UNRESERVE_ITEM_ERROR: 'UNRESERVE_ITEM_ERROR',
 };
 
 export const addItem = formData => async dispatch => {
@@ -23,16 +25,31 @@ export const addItem = formData => async dispatch => {
     const res = await api.post('/items', formData);
     dispatch({
       type: types.ADD_ITEM,
-      payload: res.data
+      payload: res.data,
     });
     return true;
   } catch (err) {
     // do something with error
     // console.log(err);
     dispatch({
-      type: types.ADD_ITEM_ERROR
+      type: types.ADD_ITEM_ERROR,
     });
     return false;
+  }
+};
+
+export const getItem = _id => async dispatch => {
+  try {
+    const res = await api.get('/items/_id');
+    dispatch({
+      type: types.GET_ITEM,
+      payload: res.data,
+    });
+  } catch (err) {
+    // console.log(err);
+    dispatch({
+      type: types.GET_ITEM_ERROR,
+    });
   }
 };
 
@@ -42,13 +59,13 @@ export const getItems = () => async dispatch => {
 
     dispatch({
       type: types.GET_ITEMS,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     // do something with error
     // console.log(err);
     dispatch({
-      type: types.GET_ITEMS_ERROR
+      type: types.GET_ITEMS_ERROR,
     });
   }
 };
@@ -61,13 +78,13 @@ export const getProviderItems = _id => async dispatch => {
 
     dispatch({
       type: types.GET_PROVIDER_ITEMS,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     // do something with error
     // console.log(err);
     dispatch({
-      type: types.GET_ITEMS_ERROR
+      type: types.GET_ITEMS_ERROR,
     });
   }
 };
@@ -80,13 +97,13 @@ export const getCollectorItems = _id => async dispatch => {
 
     dispatch({
       type: types.GET_COLLECTOR_ITEMS,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     // do something with error
     // console.log(err);
     dispatch({
-      type: types.GET_ITEMS_ERROR
+      type: types.GET_ITEMS_ERROR,
     });
   }
 };
@@ -97,32 +114,32 @@ export const getMyItems = () => async dispatch => {
 
     dispatch({
       type: types.GET_MY_ITEMS,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     // do something with error
     // console.log(err);
     dispatch({
-      type: types.GET_MY_ITEMS_ERROR
+      type: types.GET_MY_ITEMS_ERROR,
     });
   }
 };
 
 export const deleteItem = _id => ({
   type: types.DELETEITEM,
-  _id
+  _id,
 });
 
 export const sortByItem = menuItem => ({
   type: types.SORTITEM,
-  menuItem
+  menuItem,
 });
 
 export const updateCollectionDates = (_id, availability) => ({
   type: types.UPDATE_COLLECTDATES,
   _id,
   availability,
-  success: true
+  success: true,
 });
 
 export const reserveItem = _id => async dispatch => {
@@ -131,13 +148,13 @@ export const reserveItem = _id => async dispatch => {
 
     dispatch({
       type: types.RESERVE_ITEM,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     // do something with error
     // console.log(err);
     dispatch({
-      type: types.GET_ITEMS_ERROR // TODO set the correct ERROR FOR UPDATE
+      type: types.GET_ITEMS_ERROR, // TODO set the correct ERROR FOR UPDATE
     });
   }
 };
@@ -148,11 +165,11 @@ export const unreserveItem = _id => async dispatch => {
 
     dispatch({
       type: types.UNRESERVE_ITEM,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     dispatch({
-      type: types.UNRESERVE_ITEM_ERROR // TODO set the correct ERROR FOR UPDATE
+      type: types.UNRESERVE_ITEM_ERROR, // TODO set the correct ERROR FOR UPDATE
     });
   }
 };
--- src/containers/Items/AddEditItem.js
+++ src/containers/Items/AddEditItem.js
@@ -1,17 +1,16 @@
 import { connect } from 'react-redux';
 import AddEditItem from 'components/Items/AddEditItem';
-import { addItem } from 'actions/item';
+import { addItem, getItem } from 'actions/item';
 
 const mapStateToProps = state => ({
   isLoggingIn: state.auth.isLoggingIn,
   loginError: state.auth.loginError,
-  isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 const mapDispatchToProps = dispatch => ({
-
-  addItem: formData => dispatch(addItem(formData))
-
+  addItem: formData => dispatch(addItem(formData)),
+  getItem: () => dispatch(getItem()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddEditItem);
