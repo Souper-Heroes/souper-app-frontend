b4b8f217bda1ed67cfff604289798cfcb8286531
Abi Sidford <asidford@gmail.com>
2020-10-21 12:34:03 +0100

    populate the component with an item to edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import {
-  Route, Switch, Redirect, HashRouter as Router
+  Route,
+  Switch,
+  Redirect,
+  HashRouter as Router,
 } from 'react-router-dom';
 import { connect } from 'react-redux';
 import TestData from 'assets/data/TestData.json';
@@ -60,7 +63,15 @@ function App(props) {
           isVerifying={isVerifying}
         />
         <ProtectedRoute
+          exact
           path={ROUTES.ADD_EDIT_ITEM}
+          component={AddEditItem}
+          isAuthenticated={isAuthenticated}
+          isVerifying={isVerifying}
+        />
+        <ProtectedRoute
+          exact
+          path={ROUTES.EDIT_ITEM}
           component={AddEditItem}
           isAuthenticated={isAuthenticated}
           isVerifying={isVerifying}
