28bcbcc3881b374947f9bdacbebb35c8bcdfc08d
cavalost <claudina.avalos@opendeusto.es>
2020-10-22 19:57:40 +0100

    fezt:fix errors

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -12,7 +12,7 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import { getAddressProfile } from 'actions/user';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles(styles);
 
