22d36cc123d039688ad4c7c9c8a7e95a2832582b
Andy Styles <Styles@caytonslaw.com>
2020-10-26 08:48:22 +0000

    pickup items from new search array

--- src/containers/Items/ItemViewPage.js
+++ src/containers/Items/ItemViewPage.js
@@ -4,29 +4,31 @@ import ItemViewPage from 'components/Items/ItemViewPage';
 import {
   updateCollectionDates,
   unreserveItem,
-  reserveItem,
+  reserveItem
 } from 'actions/item';
 
 const getItem = (state, ownProps) => {
   if (ownProps.match.params.type === 'search') {
-    return state.item.search ? state.item.search.find(i => i._id === ownProps.match.params.id) : null;
+    return state.item.search.paginatedResults
+      ? state.item.search.paginatedResults.find(
+        i => i._id === ownProps.match.params.id
+      )
+      : null;
   }
-  return state.item.myitems ? state.item.myitems.find(i => i._id === ownProps.match.params.id) : null;
+  return state.item.myitems
+    ? state.item.myitems.find(i => i._id === ownProps.match.params.id)
+    : null;
 };
 
-const mapStateToProps = (state, ownProps) => (
-  {
-    isLoggingIn: state.auth.isLoggingIn,
-    isAuthenticated: state.auth.isAuthenticated,
-    _id: state.auth.user.uid, // Get the actual uuid from firebase of the logged in user
-    id: ownProps.match.params.id,
-    myitems: state.item.myitems,
-    type: ownProps.match.params.type,
-    // item: ownProps.match.params.type !== 'search' ? state.item.myitems ? state.item.myitems.find(i => i._id === ownProps.match.params.id) : null
-    //  : state.item.search ? state.item.search.find(i => i._id === ownProps.match.params.id) : null
-    item: getItem(state, ownProps)
-  }
-);
+const mapStateToProps = (state, ownProps) => ({
+  isLoggingIn: state.auth.isLoggingIn,
+  isAuthenticated: state.auth.isAuthenticated,
+  _id: state.auth.user.uid, // Get the actual uuid from firebase of the logged in user
+  id: ownProps.match.params.id,
+  myitems: state.item.myitems,
+  type: ownProps.match.params.type,
+  item: getItem(state, ownProps)
+});
 
 const mapDispatchToProps = dispatch => ({
   updateCollectionDates: (_id, availiability) =>
@@ -34,7 +36,7 @@ const mapDispatchToProps = dispatch => ({
     dispatch(updateCollectionDates(_id, availiability)),
 
   unreserveItem: _id => dispatch(unreserveItem(_id)),
-  reserveItem: _id => dispatch(reserveItem(_id)),
+  reserveItem: _id => dispatch(reserveItem(_id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ItemViewPage);
